ae047cc970ddd90be332e2a6d3c8f683c129ccf7
ARKUN007 <avinashkumargenious@gmail.com>
2026-03-09 18:44:11 +0000

    Initial commit to save file

---
+++ Inheritance.py
@@ -0,0 +1,19 @@
+class Person():
+    def __init__(self, fname, lname):
+        self.firstname =fname
+        self.lastname = lname
+
+    def printname(self):
+        print(self.firstname, self.lastname)
+p1 = Person("Avinash", "Kumar")
+p1.printname()
+
+class Student(Person):
+    def __init__(self, fname, lname, year):
+        super().__init__(fname, lname)
+        self.graduationyear = year
+
+    def welcome(self):
+        print("Welcome", self.firstname, self.lastname, "graduated in ", self.graduationyear)
+s1 = Student("Avinash", "Kumar", 2020)
+s1.welcome()
