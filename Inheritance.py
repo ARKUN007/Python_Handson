7a59c20a3b70f035cec4a5b27b5adf2140caf019
ARKUN007 <avinashkumargenious@gmail.com>
2026-03-09 18:47:57 +0000

    Push to main

--- Inheritance.py
+++ Inheritance.py
@@ -8,6 +8,7 @@ class Person():
 p1 = Person("Avinash", "Kumar")
 p1.printname()
 
+# Below is the child class inheriting the Person class property
 class Student(Person):
     def __init__(self, fname, lname, year):
         super().__init__(fname, lname)
