a904bf87cbd6760ff6fc54875a5288b9ddd9df90
ARKUN007 <avinashkumargenious@gmail.com>
2026-03-10 16:38:49 +0000

    added set_age() method

--- Encapsulation1.py
+++ Encapsulation1.py
@@ -4,11 +4,13 @@ class Person:
         self.__age = age #private property is age
     def get_age(self):
         return self.__age
+    def set_age(self,age):
+        self.__age = age
 p1 = Person("Avinash", 29)
 print(p1.name)
 #print(p1.age) this line will throw error because we have set age as provate
 
-#making setter() method
+print(p1.get_age()) # so we have to call get_age() to see the age
 
-# so we have to call get_age() to see the age
-print(p1.get_age())
+p1.set_age(30) #changing the age with set_age() method
+print(p1.get_age()) #now it will print new age
