9556e7f0e3e06ce6b49b2e26d719e5f2cb86e026
ARKUN007 <avinashkumargenious@gmail.com>
2026-03-10 14:49:07 +0000

    addded comment for the setter() method

--- Encapsulation1.py
+++ Encapsulation1.py
@@ -8,5 +8,7 @@ p1 = Person("Avinash", 29)
 print(p1.name)
 #print(p1.age) this line will throw error because we have set age as provate
 
+#making setter() method
+
 # so we have to call get_age() to see the age
 print(p1.get_age())
